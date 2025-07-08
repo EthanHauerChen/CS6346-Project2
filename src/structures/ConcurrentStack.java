dc81a4eba2f0961d8370afab2683da289e597edc
Ethan <ethanhchen01@gmail.com>
2025-07-07 22:01:10 -0500

    contains a concurrent stack that is probably correct, but done very trivially and probably not in the way the assignment intended

--- src/structures/ConcurrentStack.java
+++ src/structures/ConcurrentStack.java
@@ -1,7 +1,10 @@
 package structures;
 
+import java.util.concurrent.locks.*;
+
 public class ConcurrentStack {
     Node top;
+    Object lock = new Object();
     private class Node {
         private Integer value;
         private Node below;
@@ -14,30 +17,47 @@ public class ConcurrentStack {
     }
 
     public void push(int value) {
-        if (top != null) {
-            Node n = new Node(value, top);
-            top = n;
-        }
-        else {
-            top = new Node(value, null);
+        synchronized(lock) {
+            if (top != null) {
+                Node n = new Node(value, top);
+                top = n;
+            }
+            else {
+                top = new Node(value, null);
+            }
         }
     }
 
     public Integer pop() {
-        Node value = top;
-        if (top.below == null) top = null;
-        else top = top.below;
+        synchronized(lock) {
+            Node value = top;
+            if (value == null) return null;
+            if (top.below == null) top = null;
+            else top = top.below;
 
-        return value != null ? value.getValue() : null;
+            return value.getValue();
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ConcurrentStack stack = new ConcurrentStack();
-        for (int i = 0; i < 10; i++) {
-            stack.push(i);
+
+        //test if correct number of ints placed in stack
+        Thread[] threads = new Thread[16];
+        int NUM_ITERATIONS = 10000;
+        for (int i = 0; i < 16; i++) {
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < NUM_ITERATIONS; j++) {
+                    stack.push(0);
+                }
+            });
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println(stack.pop());
-        }
+        for (int i = 0; i < 16; i++) threads[i].start();
+        for (int i = 0; i < 16; i++) threads[i].join();
+
+        int count = 0;
+        while(stack.pop() != null) count++;
+
+        System.out.println(count);
     }
 }
