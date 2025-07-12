8339a34559278acd4cde835167ca1e232de2932b
Ethan <ethanhchen01@gmail.com>
2025-07-11 22:58:27 -0500

    added test for checking that removals are mutually exclusive

--- src/tests/ConcurrentBSTTest.java
+++ src/tests/ConcurrentBSTTest.java
@@ -3,7 +3,9 @@ package tests;
 import common.TestUtil;
 import structures.ConcurrentBST;
 
+import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class ConcurrentBSTTest extends AbstractTest {
 
@@ -64,5 +66,37 @@ public class ConcurrentBSTTest extends AbstractTest {
 
            } catch (InterruptedException e) {}
         });
+
+        it("should have mutually exclusive removals", () -> {
+            try {
+                int NUM_THREADS = 1000;
+                Thread[] threads = new Thread[NUM_THREADS];
+
+                ArrayList<Integer> numbers = TestUtil.getListOfNumbers(0, NUM_THREADS);
+                ArrayList<Integer> shuffledNumbers = TestUtil.getShuffledNumbers(numbers);
+                HashSet<Integer> leftOverNumbers = new HashSet<>(shuffledNumbers.subList((int) (NUM_THREADS * 0.75), NUM_THREADS));
+                HashSet<Integer> bstSet; //set that will contain the numbers in bst after all removal operations
+                boolean correctItemsLeftInBST = false;
+
+                for (int i = 0; i < NUM_THREADS; i++) {
+                    int value = numbers.get(i);
+                    bst.add(value);
+                }
+
+                for (int i = 0; i < (int) (NUM_THREADS * 0.75); i++) { //only remove a portion of numbers in bst
+                    int value = shuffledNumbers.get(i);
+                    threads[i] = new Thread(() -> bst.remove(value));
+                }
+
+                TestUtil.runThreads(Arrays.copyOfRange(threads, 0, (int) (NUM_THREADS * 0.75)));
+                TestUtil.joinThreads(Arrays.copyOfRange(threads, 0, (int) (NUM_THREADS * 0.75)));
+                bstSet = new HashSet<>(bst.toArrayList());
+                if (leftOverNumbers.equals(bstSet)) correctItemsLeftInBST = true;
+                System.out.println(bst.toArrayList());
+                expect(bst.toArrayList().size() == (int) (NUM_THREADS * 0.25)
+                && correctItemsLeftInBST);
+
+            } catch (InterruptedException e) {}
+            });
     }
 }
