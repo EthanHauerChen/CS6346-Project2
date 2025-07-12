a23612f4cb8e90a9d34425171bed2b6b0c4be2bc
haofast <67030908+haofast@users.noreply.github.com>
2025-07-11 19:34:27 -0500

    saving work

--- src/tests/ConcurrentBSTTest.java
+++ src/tests/ConcurrentBSTTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import common.TestUtil;
 import structures.ConcurrentBST;
 
 import java.util.ArrayList;
@@ -23,28 +24,45 @@ public class ConcurrentBSTTest extends AbstractTest {
                 int NUM_THREADS = 5000;
                 Thread[] threads = new Thread[NUM_THREADS];
 
-                for (int i = 0; i < NUM_THREADS; i++) {
-                    int key = i + 1;
-                    threads[i] = new Thread(() -> bst.add(key));
-                }
+                ArrayList<Integer> numbers = TestUtil.getListOfNumbers(0, NUM_THREADS);
+                ArrayList<Integer> shuffledNumbers = TestUtil.getShuffledNumbers(numbers);
 
                 for (int i = 0; i < NUM_THREADS; i++) {
-                    threads[i].start();
+                    int value = shuffledNumbers.get(i);
+                    threads[i] = new Thread(() -> bst.add(value));
                 }
 
-                for (int i = 0; i < NUM_THREADS; i++) {
-                    threads[i].join();
-                }
+                TestUtil.runThreads(threads);
+                TestUtil.joinThreads(threads);
+                expect(bst.toArrayList().equals(numbers));
 
-                ArrayList<Integer> expectedList = new ArrayList<>();
+            } catch (InterruptedException _) {}
+        });
 
-                for (int i = 0; i < NUM_THREADS; i++) {
-                    expectedList.add(i + 1);
-                }
+        it("should remove all items from the list", () -> {
+           try {
+               int NUM_THREADS = 1000;
+               Thread[] threads = new Thread[NUM_THREADS];
 
-                expect(bst.toArrayList().equals(expectedList));
+               ArrayList<Integer> numbers = TestUtil.getListOfNumbers(0, NUM_THREADS);
+               ArrayList<Integer> shuffledNumbers = TestUtil.getShuffledNumbers(numbers);
 
-            } catch (InterruptedException e) {}
+               for (int i = 0; i < NUM_THREADS; i++) {
+                   int value = numbers.get(i);
+                   bst.add(value);
+               }
+
+               for (int i = 0; i < NUM_THREADS; i++) {
+                   int value = shuffledNumbers.get(i);
+                   threads[i] = new Thread(() -> bst.remove(value));
+               }
+
+               TestUtil.runThreads(threads);
+               TestUtil.joinThreads(threads);
+               System.out.println(bst.toArrayList());
+               expect(bst.toArrayList().equals(new ArrayList<>()));
+
+           } catch (InterruptedException _) {}
         });
     }
 }
