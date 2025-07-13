16727aafc6e3b58defe8f9791556876b8ddcc40a
Ethan <ethanhchen01@gmail.com>
2025-07-12 20:24:33 -0500

    stack now uses a semaphore instead of Java's synchronize

--- src/structures/ConcurrentStack.java
+++ src/structures/ConcurrentStack.java
@@ -4,10 +4,11 @@ import java.util.concurrent.locks.*;
 import common.IListLikeDataStructure;
 
 import java.util.HashSet;
+import java.util.concurrent.Semaphore;
 
 public class ConcurrentStack implements IListLikeDataStructure {
     Node top;
-    Object lock = new Object();
+    Semaphore mutex = new Semaphore(1);
     private class Node {
         private Integer value;
         private Node below;
@@ -19,109 +20,44 @@ public class ConcurrentStack implements IListLikeDataStructure {
         public Integer getValue() { return value; }
     }
 
-    public void push(int value) {
-        synchronized(lock) {
-            if (top != null) {
-                Node n = new Node(value, top);
-                top = n;
-            }
-            else {
-                top = new Node(value, null);
-            }
+    public void push(int value) throws InterruptedException {
+        mutex.acquire();
+        if (top != null) {
+            Node n = new Node(value, top);
+            top = n;
         }
+        else {
+            top = new Node(value, null);
+        }
+        mutex.release();
     }
 
-    public Integer pop() {
-        synchronized(lock) {
-            Node value = top;
-            if (value == null) return null;
-            if (top.below == null) top = null;
-            else top = top.below;
+    public Integer pop() throws InterruptedException {
+        mutex.acquire();
+        Node value = top;
+        if (value == null) return null;
+        if (top.below == null) top = null;
+        else top = top.below;
+        mutex.release();
 
-            return value.getValue();
-        }
+        return value.getValue();
     }
 
     public boolean search(int key) { return false; } //no peek operation implemented, so search only exists because this class implements IListLikeDataStructure
 
     public boolean add(int key) {
-        this.push(key);
-        return true;
+        try {
+            this.push(key);
+            return true;
+        } catch (InterruptedException e) { return false; }
     }
 
     public boolean remove(int key) { //pop operation does not use a key
-        return pop() != null;
+        try {
+            return pop() != null;
+        } catch (InterruptedException e) { return false; }
     }
 
     //not required, exists only for convenience during testing
     public void clear() { top = null; }
-
-    public static void main(String[] args) throws InterruptedException {
-        ConcurrentStack stack = new ConcurrentStack();
-
-        //test if correct number of ints placed in stack
-        Thread[] threads = new Thread[16];
-        int NUM_ITERATIONS = 10000;
-        for (int i = 0; i < 16; i++) {
-            threads[i] = new Thread(() -> {
-                for (int j = 0; j < NUM_ITERATIONS; j++) {
-                    stack.push(0);
-                }
-            });
-        }
-        for (int i = 0; i < 16; i++) threads[i].start();
-        for (int i = 0; i < 16; i++) threads[i].join();
-
-        int count = 0;
-        while(stack.pop() != null) count++;
-
-        System.out.println(count);
-
-        //test if correct number of ints popped from stack. stack should be empty at the end
-        stack.clear();
-        for (int i = 0; i < NUM_ITERATIONS * threads.length; i++) stack.push(0);
-        for (int i = 0; i < 15; i++) {
-            threads[i] = new Thread(() -> {
-                for (int j = 0; j < NUM_ITERATIONS; j++) {
-                    stack.pop();
-                }
-            });
-        }
-        threads[threads.length-1] = new Thread(() -> {
-            for (int i = 0; i < NUM_ITERATIONS-1; i++) stack.pop();
-        });
-        //at this point, stack should have 1 element left
-        for (int i = 0; i < 16; i++) threads[i].start();
-        for (int i = 0; i < 16; i++) threads[i].join();
-
-        //should print "true" if num of stack pops is correct
-        System.out.println(stack.pop() != null && stack.pop() == null);
-
-        //test if all items were placed in the stack
-        HashSet<Integer> itemsInStack = new HashSet<>();
-        
-        for (int i = 0; i < NUM_ITERATIONS * threads.length; i++) itemsInStack.add(i);
-        stack.clear();
-        for (int i = 0; i < 16; i++) {
-            int tempI = i;
-            threads[i] = new Thread(() -> {
-                for (int j = 0; j < NUM_ITERATIONS; j++) {
-                    stack.push(tempI * NUM_ITERATIONS + j);
-                }
-            });
-        }
-        //at this point, stack should contain all integers from 0 to NUM_ITERATIONS*threads.length
-        for (int i = 0; i < 16; i++) threads[i].start();
-        for (int i = 0; i < 16; i++) threads[i].join();
-
-        boolean allItemsInStack = true;
-        for (int i = 0; i < NUM_ITERATIONS * threads.length; i++) {
-            allItemsInStack = itemsInStack.remove(stack.pop());
-            if (!allItemsInStack) {
-                System.out.println("Item popped off stack that shouldn't exist in the stack");
-                break;
-            }
-        }
-        System.out.println("All items on stack are accounted for and stack is empty: " + (allItemsInStack && itemsInStack.isEmpty() && stack.pop() == null));
-    }
 }
